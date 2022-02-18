861f941ab247db0cbda2e4e328aa27f5318f9e39
Anton Hibl <antonhibl11@gmail.com>
2022-02-17 23:13:43 -0700

    Adding WebAssembly for later testing

---
+++ assets/webassembly/as_add.ts
@@ -0,0 +1,1 @@
+export function AddInts(a: i32, b: i32): i32 { return a+b; }
---
+++ assets/webassembly/hello_world.ts
@@ -0,0 +1,3 @@
+declare function console_log( msg: string ):void;export function
+HelloWorld():void { console_log("hello world!");
+}
