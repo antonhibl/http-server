5c20e24a6035fbb1c79dd51f4093b22172cfa201
Anton Hibl <antonhibl11@gmail.com>
2022-02-17 20:16:36 -0700

    adding more comments for clarity

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module www.github.com/antonhibl/http-server
 
 go 1.17
+
+require github.com/awoodbeck/gnp v0.0.0-20210112195907-15e350dd6b92 // indirect
--- http-server.go
+++ http-server.go
@@ -17,7 +17,7 @@ func TestSimpleHTTPServer(t *testing.T) {
 	// Defining basic Server properties
 	srv := &http.Server{
 		// The Server's Address
-		addr: "127.0.0.1:8081",
+		Addr: "127.0.0.1:8081",
 		// Some default handlers
 		// http.TimeoutHandler() is a middleware function before requests are sent to http.DefaultHandler()
 		Handler: http.TimeoutHandler(
@@ -35,58 +35,78 @@ func TestSimpleHTTPServer(t *testing.T) {
 	// Define a serve method which returns http.ErrServerClosed when exiting normally
 	// mainly it serves requests from the listener
 	go func() {
-		err := srv.Serve(1)
+		err := srv.Serve(l)
 		if err != http.ErrServerClosed {
 			t.Error(err)
 		}
 	}()
 
+	// Setting a struct for HTTP Request Test Cases
 	testCases := []struct {
 		method   string
 		body     io.Reader
 		code     int
 		response string
 	}{
+		// GET Method
 		{http.MethodGet, nil, http.StatusOK, "Hello, friend."},
+		// POST Method
 		{http.MethodPost, bytes.NewBufferString("<world>"),
 			http.StatusOK,
 			"Hello, &lt;world&gt;!"},
+		// http Headers
 		{http.MethodHead, nil, http.StatusMethodNotAllowed, ""},
 	}
 
+	// establishing a client
 	client := new(http.Client)
+	formatting the filepath in the server
 	path := fmt.Sprintf("http://%s/", srv.Addr)
 
+	// iterate over testcases
 	for i, c := range testCases {
+		// establish the new request
 		r, err := http.NewRequest(c.method, path, c.body)
+
+		// If there is an error, report it
 		if err != nil {
 			t.Errorf("%d: %v", i, err)
 			continue
 		}
 
+		// tell client to perform some request
 		resp, err := client.Do(r)
+
+		// if there is an error, report it
 		if err != nil {
 			t.Errorf("%d: %v", i, err)
 			continue
 		}
 
+		// If an unexpected status is returned
 		if resp.StatusCode != c.code {
 			t.Errorf("%d: unexpected status code: %q", i, resp.Status)
 		}
 
+		// read the body contents if not empty
 		b, err := ioutil.ReadAll(resp.Body)
+
+		// If there is an error report it
 		if err != nil {
 			t.Errorf("%d: %v", i, err)
 			continue
 		}
 
+		// close the body
 		_ = resp.Body.Close()
 
+		// test if response is a string, if not report error
 		if c.response != string(b) {
 			t.Errorf("%d: expected %q; actual %q", i, c.response, b)
 		}
 	}
 
+	// close the server and check for an error in doing so
 	if err := srv.Close(); err != nil {
 		t.Fatal(err)
 	}
